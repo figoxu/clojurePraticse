7368b11ebc4797f4a74f5b3d3863cea4519df028
figoxu <xujh945@qq.com>
2020-01-30 17:49:49 +0800

    init snake project

---
+++ snake/src/snake/core.clj
@@ -0,0 +1,14 @@
+(ns snake.core (:gen-class))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+
+(defn -main
+  [& args]
+  (println "Hello, World!")
+  (foo "figo")
+  )
