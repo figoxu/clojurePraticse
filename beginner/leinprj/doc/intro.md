51b19b020091db5383c461e48d958edf22f250bf
figoxu <xujh945@qq.com>
2020-09-26 11:21:14 +0800

    init

---
+++ beginner/leinprj/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to leinprj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ beginner/leinprj/test/leinprj/core_test.clj
@@ -0,0 +1,7 @@
+(ns leinprj.core-test
+  (:require [clojure.test :refer :all]
+            [leinprj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
