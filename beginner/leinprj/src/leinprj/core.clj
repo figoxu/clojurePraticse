85cea987fda20a61cf905dfd3f44c04abb769da0
figoxu <xujh945@qq.com>
2020-09-26 12:04:49 +0800

    try money

--- beginner/leinprj/src/leinprj/core.clj
+++ beginner/leinprj/src/leinprj/core.clj
@@ -1,6 +1,16 @@
 (ns leinprj.core)
+(require '[clojurewerkz.money.amounts :as ma])
+(require '[clojurewerkz.money.currencies :as mc])
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+
+(defn tryMoney
+  "金额相关的联系"
+  []
+  (def two (ma/amount-of mc/USD 2))
+  (ma/total [two two two two])
+  )
